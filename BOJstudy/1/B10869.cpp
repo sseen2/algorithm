6753563fdb0ddb7b716ec395718a4cb98964ade1
sseen2 <59137639+sseen2@users.noreply.github.com>
2023-07-09 19:55:40 +0900

    boj

---
+++ BOJstudy/1/B10869.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << "\n" << a-b << "\n" << a*b << "\n" << a/b << "\n" << a%b << "\n";
+}
+
+// ************************************
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+  solve();
+}
--- BOJstudy/1/B10871.cpp
+++ BOJstudy/1/B10871.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int n, x;
     cin >> n >> x;
 
