3a56b4329cd7dd7c79f26f5ce958af480f6e30d7
sseen2 <59137639+sseen2@users.noreply.github.com>
2024-01-07 23:09:25 +0900

    boj

--- BOJstudy/12/B1600.cpp
+++ BOJstudy/12/B1600.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 #define all(x) x.begin(), x.end()
 #define ll long long
-#define X first
-#define Y second
+// #define X first
+// #define Y second
+#define X get<0>
+#define Y get<1>
+#define Z get<2>
 
 typedef pair<int, int> pi;
 typedef pair<ll,ll> pl;
@@ -14,16 +17,24 @@ const char nl = '\n';
 
 int k, h, w;
 int board[205][205]; // h, w
-int dist[205][205][30];
+int dist[205][205][2]; // 0: 말로 움직일 수 있음, 1: 말로 움직일 수 없음 
 int kdx[] = {1, 2, 2, 1, -1, -2, -2, -1};
 int kdy[] = {2, 1, -1, -2, -2, -1, 1, 2};
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 
-void print() {
+void print1() {
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++)
-            cout << dist[i][j] << " ";
+            cout << dist[i][j][0] << " ";
+        cout << nl;
+    }
+}
+
+void print2() {
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++)
+            cout << dist[i][j][1] << " ";
         cout << nl;
     }
 }
@@ -33,24 +44,30 @@ void solve() {
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             cin >> board[i][j];
-            dist[i][j] = -1;
+            dist[i][j][0] = -1;
+            dist[i][j][1] = -1;
         }
     }
 
-    queue<pi> q;
+    queue<tuple<int,int,int>> q;
     dist[0][0][0] = 0;
-    q.push({0, 0});
+    q.push(make_tuple(0, 0, k));
     while (!q.empty()) {
         auto cur = q.front(); q.pop();
+        int x,y,z;
+        tie(x,y,z) = cur;
+        int horse = Z(cur);
+        if (horse > 0) {
+            
+        }
         for (int i = 0; i < 8; i++) {
-            int nx = kdx[i] + cur.X;
-            int ny = kdy[i] + cur.Y;
+            int nx = kdx[i] + X(cur);
+            int ny = kdy[i] + Y(cur);
             if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
             if (dist[nx][ny] > -1 || board[nx][ny] == 1) continue;
             dist[nx][ny] = dist[cur.X][cur.Y] + 1;
             q.push({nx, ny});
         }
-        k--;
     }
 
     cout << dist[h - 1][w - 1];
