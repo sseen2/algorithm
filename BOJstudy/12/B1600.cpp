2a01e8c471f7208957ced2e6103c6e91221852ae
sseen2 <59137639+sseen2@users.noreply.github.com>
2024-01-16 23:13:12 +0900

    boj

--- BOJstudy/12/B1600.cpp
+++ BOJstudy/12/B1600.cpp
@@ -12,7 +12,7 @@ const char nl = '\n';
 
 int k, w, h;
 int board[205][205];
-int dist[205][205][2];
+int dist[205][205][35];
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 int hx[] = {-1, -2, -2, -1, 1, 2, 2, 1};
@@ -41,14 +41,13 @@ void solve() {
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
             cin >> board[i][j];
-            dist[i][j][0] = -1;
-            dist[i][j][1] = -1;
+            fill(dist[i][j], dist[i][j] + k + 1, -1);
         }
     }
 
     queue<tuple<int, int, int>> q;
     q.push({0, 0, k});
-    dist[0][0][0] = 0;
+    dist[0][0][k] = 0;
     while (!q.empty()) {
         int x, y, z;
         tie(x, y, z) = q.front(); q.pop();
@@ -57,28 +56,18 @@ void solve() {
                 int nx = x + hx[dir];
                 int ny = y + hy[dir];
                 if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-                if (board[nx][ny] == 1 || dist[nx][ny][0] != -1) continue;
+                if (board[nx][ny] == 1 || dist[nx][ny][z - 1] != -1) continue;
                 q.push({nx, ny, z - 1});
-                dist[nx][ny][0] = dist[x][y][0] + 1;
+                dist[nx][ny][z - 1] = dist[x][y][z] + 1;
             }
         }
         for (int dir = 0; dir < 4; dir++) {
             int nx = x + dx[dir];
             int ny = y + dy[dir];
             if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-            if (board[nx][ny] == 1 || dist[nx][ny][0] != -1) continue;
-            if (z > 0) {
-                q.push({nx, ny, z});
-                dist[nx][ny][0] = dist[x][y][0] + 1;
-            }
-            else if (z == 0) {
-                q.push({nx, ny, -1});
-                dist[nx][ny][1] = dist[x][y][0] + 1;
-            }
-            else {
-                q.push({nx, ny, z});
-                dist[nx][ny][1] = dist[x][y][1] + 1;
-            } 
+            if (board[nx][ny] == 1 || dist[nx][ny][z] != -1) continue;
+            q.push({nx, ny, z});
+            dist[nx][ny][z] = dist[x][y][z] + 1;
         }
     }
     int a = dist[h - 1][w - 1][0], b = dist[h - 1][w - 1][1];
