f255fcdb87a89e2b0fabc3511cd11872428dce1e
sseen2 <kse10023@naver.com>
2024-07-24 22:50:04 +0900

    boj

---
+++ BOJstudy/dp/B1026.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(x) x.begin(), x.end()
+#define ll long long
+#define X first
+#define Y second
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+const char nl = '\n';
+const int N = 55;
+
+int n;
+int a[N];
+int b[N];
+
+void solve() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+    sort(a, a + n);
+    sort(b, b + n, greater<>());
+
+    int rs = 0;
+    for (int i = 0; i < n; i++) 
+        rs += a[i] * b[i];
+    
+    cout << rs;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    while(t--)
+        solve();
+}
