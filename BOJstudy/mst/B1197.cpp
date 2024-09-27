2ad90530465de65d1bd3ff37b1c4bdc0a4c7dcb0
sseen2 <kse10023@naver.com>
2024-09-27 21:18:02 +0900

    boj

--- BOJstudy/mst/B1197.cpp
+++ BOJstudy/mst/B1197.cpp
@@ -3,14 +3,55 @@ using namespace std;
 #define X first
 #define Y second
 typedef pair<int, int> pi;
+typedef vector<int> vi;
 const char nl = '\n';
 const int N = 10005;
+const int M = 100005;
 int n, m, cnt = 0, rs = 0;
+
+// 크루스칼 알고리즘
+vi p(N, -1);
+tuple<int, int, int> edge[M];
+
+int find (int x) {
+    if (p[x] < 0) return x;
+    return p[x] = find(p[x]);
+}
+
+bool merge(int u, int v) {
+    u = find(u); v = find(v);
+    if (u == v) return 0;
+    if (p[u] == p[v]) p[u]--;
+    if (p[u] < p[v]) p[v] = u;
+    else p[u] = v;
+    return 1;
+}
+
+void kruskal() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++) {
+        int a, b, cost; cin >> a >> b >> cost;
+        edge[i] = {cost, a, b};
+    }
+    sort(edge, edge + m);
+
+    for (int i = 0; i < m; i++) {
+        int a, b, cost;
+        tie(cost, a, b) = edge[i];
+        if (!merge(a, b)) continue;
+        rs += cost;
+        cnt++;
+        if (cnt == n - 1) break;
+    }
+
+    cout << rs;
+}
+
+// 프림 알고리즘
 vector<pi> g[N];    // 가중치, 정점
 bool chk[N];        // i번째 정점이 최소 신장 트리에 속해있는지
 
-// 프림 알고리즘
-void solve() {
+void prim() {
     cin >> n >> m;
     for (int i = 0; i < m; i++) {
         int a, b, cost; cin >> a >>  b >> cost;
@@ -43,5 +84,6 @@ void solve() {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    solve();
+    // prim();
+    kruskal();
 }
